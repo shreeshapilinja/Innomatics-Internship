76e878fbc60ba51fb0854105fe31443421419482
Shreesha B <95085686+shreeshapilinja@users.noreply.github.com>
2024-11-14 22:05:01 +0530

    Add files via upload

---
+++ Assignment_9/README.md
@@ -0,0 +1,2 @@
+## Assignment - 9
+- [https://in9245339assignment9.onrender.com/todos/](https://in9245339assignment9.onrender.com/todos/)
---
+++ Assignment_9/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// MongoDB Connection
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
+// Routes
+const todoRoutes = require('./routes/todos');
+app.use('/todos', todoRoutes);
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
